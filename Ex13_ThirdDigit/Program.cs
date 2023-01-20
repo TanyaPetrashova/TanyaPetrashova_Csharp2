7226556dbe66e31ec8468a86caef3ac769e025fa
tanya <zharik08@mail.ru>
2023-01-20 17:51:17 +0800

    Example 13 Third digit

---
+++ Ex13_ThirdDigit/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("Enter a number: ");
+string Number = Console.ReadLine();
+
+if(Number.Length > 2)
+{
+    Console.WriteLine("Third digit: " +Number[2]);
+}
+
+else
+{
+    Console.WriteLine("No third digit");
+}
+
