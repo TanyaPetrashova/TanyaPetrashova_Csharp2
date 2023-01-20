c10d34b4411fdabbad2d342724754d79726109bf
tanya <zharik08@mail.ru>
2023-01-20 20:39:33 +0800

    Example 15 Weekend

---
+++ Ex15_Weekend/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Enter a number of the day of the week: ");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
+
+DayInfo(DayNumber);
+
+void DayInfo (int DayNumber)
+{
+    if (DayNumber == 6 || DayNumber == 7)
+    {
+        Console.WriteLine("Yes");
+    }
+        else if (DayNumber < 1 || DayNumber > 7) 
+        {
+        Console.WriteLine("Not a day of the week");
+        }
+    else 
+    {  
+        Console.WriteLine("No");
+    }
+}
