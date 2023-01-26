aa6bd27fe0472df2b7b1fc542e086db731bd14df
tanya <zharik08@mail.ru>
2023-01-26 21:09:25 +0800

    Exercise 19 Palindrome

---
+++ Ex19_Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ
+
+Console.WriteLine("Enter five-digit number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int a = Number / 10000;
+int b = Number / 1000  % 10;
+int c = Number / 100  % 10;
+int d = Number / 10  % 10;
+int e = Number % 10;
+
+if (a == e)
+{
+    if (b == d)
+    {
+        Console.WriteLine("It's a palindrome");
+    }
+}
+else
+{
+    Console.WriteLine("It's not a palindrome");
+}
+
+
+
