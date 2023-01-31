d28e03d9934413b5eab3b1f40417f6b8469f636c
tanya <zharik08@mail.ru>
2023-01-31 23:45:28 +0800

    Exercise 47 Выведение двумерного массива

---
+++ Ex47_Array2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] matrix = new double[3, 4];
+// Метод печати матрицы
+void PrintMatrix(double[,] matr)              
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Метод заполнения массива случайными вещественными числами
+void FillArray(double[,] matr)                      
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;             
+        }
+    }
+}
+
+FillArray(matrix);
+PrintMatrix(matrix);
