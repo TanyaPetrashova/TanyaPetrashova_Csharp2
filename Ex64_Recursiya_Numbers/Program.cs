1ea58bba7e33f4349360c4e4a7f5d0302e444e9c
tanya <zharik08@mail.ru>
2023-02-06 01:29:48 +0800

    Переименование папки

---
+++ Ex64_Recursiya_Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64. Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Enter a number N: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+void CountNumber (int number)
+{
+    if (number <= 0) return;
+    Console.Write($"{number}, ");
+    CountNumber (number - 1);
+}
+Console.Write("--> ");
+CountNumber(number);
