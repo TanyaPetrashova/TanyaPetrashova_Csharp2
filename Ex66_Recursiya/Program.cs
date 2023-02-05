60e417db34b8cdb7d1341ff3b3ddca8780524e5f
tanya <zharik08@mail.ru>
2023-02-06 00:40:12 +0800

    Ex66 Нахождение суммы натуральных эл-тов от M до N

---
+++ Ex66_Recursiya/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66. Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Enter a natural number M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a natural number greater than M: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int sum (int numberM, int numberN)
+{
+    int valie = numberM;
+    if (numberM == numberN)
+    {
+        return 0;
+    }
+    else
+    {
+       numberM ++;
+       valie = numberM + sum(numberM, numberN);
+       return valie;
+    }
+}
+
+Console.WriteLine($"--> {sum(numberM-1, numberN)}");
